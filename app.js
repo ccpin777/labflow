beae467b5ccbe6d93406172b0553f91620cb22b8
Pin <ccpin777@hotmail.com>
2026-08-15 18:06:40 -0500

    fix experimental section/cards

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "Version 5 2026-08-14";
+const APP_VERSION = "Version 5 2026-08-15";
 const INITIAL_LABFLOW_DATA = { experiments: [] };
 const INITIAL_TEMPLATES = {};
 const INITIAL_MEASUREMENTS = { measurements: [], samples: [], cells: [] };
